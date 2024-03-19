913ca25f9a7afe4fb353c57f0786796aa0c9f5d7
Ivette Flores <jovannaivettefloreshernandez@gmail.com>
2024-03-19 12:17:54 -0500

    mejorando la configuración de cors

--- index.js
+++ index.js
@@ -12,40 +12,35 @@ dotenv.config();
 conectarDB();
 const dominiosPermitidos = [process.env.FRONTEND_URL];
 const corsOptions = {
-    // origin: function(origin, callback) {
-    //     origin = process.env.FRONTEND_URL;
-    //     if (!origin.toString()) {
-    //         // Permitir solicitudes sin origen (por ejemplo, solicitudes locales)
-    //         callback(null, true);
-    //         return;
-    //     }
+    origin: function(origin, callback) {
+        
+        if (!origin) {
+            // Permitir solicitudes sin origen (por ejemplo, solicitudes locales)
+            callback(null, true);
+            return;
+        }
 
-    //     if (dominiosPermitidos.includes(origin.toString())) {
-    //         // El origen está en la lista de dominios permitidos
-    //         callback(null, true);
-    //     } else {
-    //         // El origen no está permitido
-    //         callback(new Error('No permitido por CORS'));
-    //     }
-    // },
-    // optionsSuccessStatus: 200
-    //NO TENIA OPCIÓN EL TIEMPO SE ME AGOTABA:()
-    // "origin": "*",
-    // "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-    // "preflightContinue": false,
-    // "optionsSuccessStatus": 204
+        if (dominiosPermitidos.includes(origin)) {
+            // El origen está en la lista de dominios permitidos
+            callback(null, true);
+        } else {
+            // El origen no está permitido
+            callback(new Error('No permitido por CORS'));
+        }
+    },
+    
 };
 
-//app.use(cors(corsOptions));
-app.use((req, res, next) => {
-    //allow access to current url. work for https as well
-    res.setHeader('Access-Control-Allow-Origin',req.header('Origin'));
-    res.removeHeader('x-powered-by');
-    //allow access to current method
-    res.setHeader('Access-Control-Allow-Methods',req.method);
-    res.setHeader('Access-Control-Allow-Headers','Content-Type');
-    next();
-  })
+app.use(cors(corsOptions));
+// app.use((req, res, next) => {
+//     //allow access to current url. work for https as well
+//     res.setHeader('Access-Control-Allow-Origin',req.header('Origin'));
+//     res.removeHeader('x-powered-by');
+//     //allow access to current method
+//     res.setHeader('Access-Control-Allow-Methods',req.method);
+//     res.setHeader('Access-Control-Allow-Headers','Content-Type');
+//     next();
+//   })
 app.use('/api/veterinarios', veterinarioRoutes);
 app.use('/api/pacientes', pacienteRoutes);
 
